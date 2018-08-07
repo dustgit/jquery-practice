f6ef68cecf235bd12d1b301d50e31f0e73184389
Dustin Starke and Michael Donovan <dustin@>
2018-08-07 10:52:21 -0700

    add simple effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
 $(document).ready(function(){
+  $('#hide-button').click(function(){
+    $('#image-area-1').hide()
+  });
+  $('#show-button').click(function(){
+    $('#image-area-1').show()
+  });
+  $('#toggle-button').click(function(){
+    $('#image-area-1').toggle()
+  });
+  $('#fade-out-button').click(function(){
+    $('#image-area-2').fadeOut(500)
+  });
+  $('#fade-in-button').click(function(){
+    $('#image-area-2').fadeIn(500)
+  });
+  $('#fade-toggle-button').click(function(){
+    $('#image-area-2').fadeToggle(500)
+  });
 
+  $('#slide-out-button').click(function(){
+    $('#image-area-3').slideUp(500)
+  });
+  $('#slide-in-button').click(function(){
+    $('#image-area-3').slideDown(500)
+  });
+  $('#slide-toggle-button').click(function(){
+    $('#image-area-3').slideToggle(500)
+  });
+  // $('#fade-in-button').click(function(){
+  //   $('#image-area-1').fadeIn()
+  // });
+  // $('#fade-in-button').click(function(){
+  //   $('#image-area-1').fadeIn()
+  // });
 })
-// var layout1 =
-// [
-//   [4,4,4],
-//   [4,3,2,3],
-//   [4,4,4],
-//   [1,1,2,4,4]
-// ]
-// function createLayout(container,layoutArray) {
-//   var rows = layoutArray.length
-//   for (var r=0;r<rows;r++) {
-//     var columns = layoutArray[r].length
-//     // make the row divs
-//     $(container).append(`<div id="row-`+r+`" class="row" style="text-align:left"></div>`)
-//     // grab the row div you just made
-//     var newRow = $('#row-'+r)
-//     // put the columns in
-//     for (var c=0;c<columns;c++) {
-//       var columnWidth = layoutArray[r][c]
-//       console.log("col wid is " + columnWidth)
-//       newRow.append(`<div id="#column-`+r+`-`+c+`" class="bordered col-md-`+columnWidth+`" style="text-align:right">#column-`+r+`-`+c+`</div>`)
-//     }
-//   }
-// }
